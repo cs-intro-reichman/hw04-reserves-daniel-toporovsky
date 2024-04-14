526b509c83845a7d0fbf1198383e2257e4c0dfe7
daniel-toporovsky <daniel.toporovsky@runi.ac.il>
2024-04-14 10:24:30 +0300

    Added code from scrath + created test methods in main

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,124 @@
 public class ArrayOps {
     public static void main(String[] args) {
+        System.out.println("----------------------------");
+        
+        // findMissingInt Test 
+        System.out.println("findMissingInt Test:");
+        int [] arr = {0};
+        System.out.println("The missing integer is " + findMissingInt(arr));
+        System.out.println("----------------------------");
+        
+        // secondMaxValue Test 
+        System.out.println("secondMaxValue Test:");
+        int [] arr1 = {-202, 48, 13, 7, 8};
+        System.out.println("The second max value is " + secondMaxValue(arr1));
+        System.out.println("----------------------------");
+
+        // containsTheSameElements Test
+        System.out.println("containsTheSameElements Test:");
+        int [] array1 = new int[] {3, -4, 1, 2, 5};
+        int [] array2 = new int[] {1, 3, -4, 5};
+        if (containsTheSameElements(array1, array2) == true) {
+            System.out.println("Both arrays contain the same numbers");
+        }
+        else {
+            System.out.println("Both arrays DON'T contain the same numbers");
+        } 
+        System.out.println("----------------------------");
+        
+
+        // isSorted Test
+        System.out.println("isSorted Test:");
+        int [] sorted = new int[] {1, 1, 500};
+        if (isSorted(sorted) == true) System.out.println("Array is sorted");
+        else System.out.println("Array is NOT sorted");
+        System.out.println("----------------------------");
         
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int [] completeSet = new int [array.length + 1];
+        int missing = 0;
+        
+        for (int i = 0; i < completeSet.length; i++) {
+            completeSet[i] = i + 1;
+        }
+
+        for (int a = 0; a < completeSet.length; a++) {
+            boolean numberExists = false;
+            for (int b = 0; b < array.length; b++) {
+                if (array[b] == completeSet[a]) numberExists = true;
+            }
+            if (numberExists == false) {
+                missing = completeSet[a];
+                break;
+            } 
+        }
+        return missing;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int firstMaxValue = 0;
+        int secondMaxValue = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (firstMaxValue < array[i]) firstMaxValue = array[i];
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if ((secondMaxValue < array[i]) && (array[i] != firstMaxValue)) secondMaxValue = array[i];
+        }
+        return secondMaxValue;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        //We will wish to start going through the largest array first if exists
+        if (array1.length > array2.length) {
+            for (int i = 0; i < array1.length; i++) {
+                boolean sameElements1 = false;
+                for (int j = 0; j < array2.length; j++) {
+                    if (array1[i] == array2[j]) {
+                        sameElements1 = true;
+                        break;
+                    }
+                }
+                if (sameElements1 == false) {
+                    return false;
+                }
+            }
+        }
+        else {
+            for (int i = 0; i < array2.length; i++) {
+                boolean sameElements1 = false;
+                for (int j = 0; j < array1.length; j++) {
+                    if (array2[i] == array1[j]) {
+                        sameElements1 = true;
+                        break;
+                    }
+                }
+                if (sameElements1 == false) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        //Increasing check
+        boolean increasing = true;
+        for (int i = 0; i < (array.length - 1); i++) {
+            if (array[i] > array[i+1]) increasing = false;
+        }
+
+        //Decreasing check
+        boolean decreasing = true;
+        for (int i = 0; i < (array.length - 1); i++) {
+            if (array[i] < array[i+1]) decreasing = false;
+        }
+        
+        if (increasing == decreasing) return false;
+        else return true;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,23 +22,165 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
+        System.out.println("-----------------------------");
 
-        System.out.println("vv");
-        
+        //capVowelsLowRest Test
+        System.out.println("capVowelsLowRest Test:");
+        String text = "yellow";
+        System.out.println(capVowelsLowRest(text));
+        System.out.println("-----------------------------");
+
+        //camelCase Test
+        System.out.println("camelCase Test:");
+        String camel = "        Intro to  coMPUter      sCIEncE ";
+        System.out.println(camelCase(camel));
+        System.out.println("-----------------------------");
+
+        //allIndexOf Test
+        System.out.println("allIndexOf Test:");
+        String input3 = "MMMM";
+        char index = 'M';
+        int [] in = allIndexOf(input3, index);
+        System.out.print("{");
+        for (int i = 0; i < in.length; i++) {
+            System.out.print(in[i]);
+            if (i+1 != in.length) System.out.print(", ");
+        }
+        System.out.println("}");
+        System.out.println("-----------------------------");        
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String formated = "";
+
+        for (int i = 0; i < string.length(); i++) {
+
+            //If the char is a lowercase letter
+            if (lower(string, i)) {
+                switch (string.charAt(i)) {
+                    case 97: case 101: case 105: case 111: case 117:
+                        formated += (char)(string.charAt(i) - 32);
+                        break;
+                    default:
+                        formated += string.charAt(i);
+                        break;
+                }
+            }
+
+            //If the char is a uppercase letter 
+            else if (upper(string, i)) {
+                switch (string.charAt(i)) {
+                    case 97: case 101: case 105: case 111: case 117:
+                        formated += string.charAt(i);
+                        break;
+                    default:
+                        formated += (char)(string.charAt(i) + 32);
+                        break;
+                }
+            }
+
+            //The char is not a letter
+            else formated += string.charAt(i);
+
+        }
+        return formated;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String tmp = "";
+        int i = 0;
+        //Check if the first character is space and skip
+        while (string.charAt(i) == ' ') {
+            i++;
+            if (i >= string.length()) break;
+        }
+        
+        //Check if the first letter is upcase and add it in lowercase
+        if (upper(string, i)) {
+            tmp += (char)(string.charAt(i) + 32);                
+        }
+        else if (lower(string, i)) {
+            tmp += string.charAt(i);
+        }
+        i++;
+
+        while (i < string.length()) {
+            //Check if the next char is space and skip 
+            while (string.charAt(i) == ' ') {
+                i++;
+                if (i >= string.length()) break;
+            }
+
+            if (i >= string.length()) break;
+            
+            //Check if the letter is the first letter of the word
+            if (string.charAt(i-1) == ' '){
+                if (upper(string, i)) {
+                    tmp += string.charAt(i);
+                    i++;                
+                }
+                else if (lower(string, i)) {
+                    tmp += (char)(string.charAt(i) - 32);
+                    i++;
+                }
+            }
+                
+            if (i >= string.length()) break;
+                        
+            //Check if the letter on second+ position is lower and add to tmp
+            if (upper(string, i)) {
+                tmp += (char)(string.charAt(i) + 32);                
+            }
+            else if (lower(string, i)) {
+                tmp += i;
+            }
+            i++;
+        }
+        return tmp;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        String indexList = "";
+        int indexCount = 0;
+
+        //Counting how many indexes we have + adding indexes to string 
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                indexList += i + " ";
+                indexCount++;
+            }
+        }
+
+        //Converting string with indexes to array of strings with one index in each
+        String [] indexStrArr = new String[indexCount];
+        for (int i = 0; i < indexStrArr.length; i++) {
+            indexStrArr[i] = "";
+        }
+
+        int j = 0;
+        for (int i = 0; i < indexStrArr.length; i++) { 
+            while (indexList.charAt(j) != ' ') {
+                indexStrArr[i] += + indexList.charAt(j) - 48;
+                 j++;
+            }
+            j++;
+            if (j >= indexList.length()) break; 
+        }
+
+        //Converting string array to integer array
+        int [] indexArr = new int[indexStrArr.length];
+        for (int i = 0; i < indexStrArr.length; i++) {
+            indexArr[i] = Integer.parseInt(indexStrArr[i]);
+        }
+        return indexArr;
+    }
+
+    public static boolean upper (String string, int index) {
+        if (64 < string.charAt(index) && string.charAt(index) < 91) return true;
+        else return false;
+    }
+    public static boolean lower (String string, int index) {
+        if (96 < string.charAt(index) && string.charAt(index) < 123) return true;
+        else return false;
     }
 }
